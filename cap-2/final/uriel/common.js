572291e1633fc4529d16c15985a1b9b3c7dc3415
uriblanco <uriel.blanco@garajedeideas.com>
2022-05-18 16:39:00 +0200

    deleted

--- cap-2/final/uriel/common.js
+++
@@ -1,3 +0,0 @@
-export function sayHello(name) {
-  alert(`Hola  ${name} bienvenido nuevamente!`);
-}
--- cap-2/final/uriel/main.js
+++
@@ -1,5 +0,0 @@
-import { account1 } from './variables.js';
-import { sayHello } from './common.js';
-
-sayHello('FUNCIONAAAA');
-console.log(account1);
