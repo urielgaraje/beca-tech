42c8ae702b17c54f63d129236847d5a3120ff674
stefanyapolo <stefany.apolo@garajedeideas.com>
2022-05-18 16:36:09 +0200

    adding gitignore

---
+++ cap-2/final/easy-bank/main.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAg7SRROkSKs4YGG69Sk2Xrmbk61xqZkL4',
+  authDomain: 'easy-bank-11106.firebaseapp.com',
+  projectId: 'easy-bank-11106',
+  storageBucket: 'easy-bank-11106.appspot.com',
+  messagingSenderId: '1063275927997',
+  appId: '1:1063275927997:web:0f127ee871fabd10515ad4',
+};
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const accountsCol = collection(db, 'accounts');
+  const accountsSnapshot = await getDocs(accountsCol);
+  const accountsList = accountsSnapshot.docs.map(doc => doc.data());
+  return accountsList;
+}
+
+getCities(db).then(data => console.log(data));
