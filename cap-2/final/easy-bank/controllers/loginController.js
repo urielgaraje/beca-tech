ef20cc1e4129a5e63fe2157f87604a8f6c11f0ee
uriblanco <uriel.blanco@garajedeideas.com>
2022-05-19 18:45:53 +0200

    first assignment

--- cap-2/final/easy-bank/controllers/loginController.js
+++ cap-2/final/easy-bank/controllers/loginController.js
@@ -19,28 +19,28 @@ const loginHandler = async (event, currentAccount) => {
   const accountsRef = collection(db, 'accounts');
   const movementsRef = collection(db, 'movements');
 
-  const accQuery = query(
-    accountsRef,
-    where('username', '==', formProps.username.trim()),
-    where('pin', '==', +formProps.pin)
-  );
-
-  const accSnapshot = await getDocs(accQuery);
-
-  if (accSnapshot.empty) alert('Usuario o contraseña incorrectos!');
-
-  const doc = accSnapshot.docs[0];
-  const movQuery = query(movementsRef, where('idUser', '==', doc.id));
-  const movSnapshot = await getDocs(movQuery);
-
-  const acc = doc.data();
-  const movements = [];
-
-  movSnapshot.forEach(doc => {
-    const { amount, date } = doc.data();
-
-    movements.push({ amount, date });
-  });
+  /**
+   * Tienen las referencias a cada coleccion tanto los movimientos como las cuentas
+   * 
+   * Los movimientos estan relacionados por su campo 'idUser' el cual pueden obtener de la cuenta en su propiedad 'id'
+   * 
+   * Los pasos serían los siguientes:
+   * 
+   * 1. Recuperan la cuenta en base a su 'username' y su 'pin' de la coleccion de accounts
+   * 2. Una vez tienen la cuenta (en caso de que exista) tienen que traer de la coleccion de 'movements'
+   *    todos los registros con el mismo 'idUser' que el de la cuenta (pueden obtener el id desdel el mismo doc).
+   * 3. Como pueden ver abajo si pudieron obtener el 'account' y la lista de 'movements' luego lo que hago 
+   *    es crear un objeto Account.
+   * 
+   * No se preocupen por todo lo demás de momento que si consiguen estos datos todo funciona correctamente!
+   * 
+   * Pista: Ya tienen todas las importaciones que necesitarían...args
+   * Docs: https://firebase.google.com/docs/firestore/query-data/queries?hl=es&authuser=2#execute_a_query
+   *       https://firebase.google.com/docs/firestore/query-data/get-data?authuser=2&hl=es
+   * 
+   * Como las fechas firebase las guarda en miliseconds ya corregí el comportamiento en el viewController todo funciona,
+   * chequeenlo para que vean como esta hecho.
+   */
 
   currentAccount = new Account(
     acc.id,
